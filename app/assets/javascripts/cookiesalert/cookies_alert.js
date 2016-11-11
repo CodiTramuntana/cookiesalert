f2513c21e9fa3b4cce5a611c2a2711fb1eef5cc3
Isaac <isaac.mg@coditramuntana.com>
2016-11-11 07:34:46 -0500

    Update cookies_alert.js

--- app/assets/javascripts/cookiesalert/cookies_alert.js
+++ app/assets/javascripts/cookiesalert/cookies_alert.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-var cookies = {
-  div : '#cookies',
-  btn : '#cookies_btn',
-  check : function (div, btn) {
-    cookies.div = div || cookies.div;
-    cookies.btn = btn || cookies.btn;
-    if (Cookies.get('cookies_alert') === undefined) {
-      $(cookies.div).css({ visibility : 'visible' });
-      $(cookies.btn).click(function () {
-        cookies.accept();
-      });
-    } else $(cookies.div).css({ visibility : 'hidden' });
-  },
-  accept : function () {
-    Cookies.set('cookies_alert','true');
-    $(cookies.div).css({ visibility : 'hidden' });
-  },
-  remove : function () {
-    Cookies.remove('cookies_alert');
-  }
-};
-=======
 var cookies = {
   div : "#cookies",
   btn : "#cookies_btn",
@@ -47,4 +24,3 @@ var cookies = {
     Cookies.remove('cookies_alert');
   }
 }
->>>>>>> 411510f38d1070d4293c711f555958e734a093ea
